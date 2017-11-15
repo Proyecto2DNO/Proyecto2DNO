e4fea3e85c75ece8ded8a4906129f28324c3a4e0
NicolasTapia <32137964+NicolasTapia@users.noreply.github.com>
2017-11-15 17:33:17 +0100

    Add files via upload

--- ColegioDNO/index.php
+++ ColegioDNO/index.php
@@ -1,4 +1,22 @@
+<? include("seguridad.php"); ?> 
+<?php
+ini_set('session.gc_maxlifetime', 3*60*60); // 3 hours
+ini_set('session.gc_probability', 1);
+ini_set('session.gc_divisor', 100);
+ini_set('session.cookie_secure', true);
+ini_set('session.use_only_cookies', true);
+session_start();
+ 
+/* Cerrar sesión */
+if(isset($_POST['cerrar_sesion'])) {
+    session_destroy();
+}
+?> 
+
+
 <!DOCTYPE html>
+
+
 <html lang="en">
 
 <head>
@@ -9,14 +27,23 @@
   <meta name="author" content="">
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="css/menu.css">
+  <link rel="stylesheet" href="css/4-col-portafolio.css">
 
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway">
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-  <title>ColegioDNO - Lista de Recursos</title>
+  <title>ColegioDNO</title>
 
 
-  <!-- Bootstrap core CSS -->
-
+<div id='cssmenu'>
+<ul>
+   <li><a href='#'>Inicio</a></li>
+   <li class='active'><a href='#'>Mis reservas</a>
+     
+   </li>
+  <li> <a href='Sesiones/index.php'>Cerrar sesión</a></li>
+   <li><a href=""><?php echo "Bienvenido " . $_SESSION['usuario_nombre']; ?></a></li>
+</ul>
+</div>
 
   <!-- Custom styles for this template -->
   <link href="css/4-col-portfolio.css" rel="stylesheet">
@@ -37,33 +64,33 @@
       <p class="w3-text-grey">Aqui puedes filtrar los recursos</p>
     </div>
     <div class="w3-bar-block">
-    	<form name="buscador" action="index_proc.php" method="GET">
-	        <div class="form-group w3-bar-item">
-	          <label for="usr">Nombre del recurso:</label>
-	          <input type="text" class="form-control" id="nombre" name="nombre">
-	          <br/>
-	          	<select class="form-control" id="tipo_recurso" name="tipo_recurso">
-		          <option disabled selected>Selecciona un tipo</option>
-		          <option>Aula de teoría</option>
-		          <option>Aula Informatica</option>
-		          <option>Portatil</option>
-		          <option>Despacho</option>
-		          <option>Carro de portátil</option>
-		          <option>Dispositivo Móvil</option>
-		          <option>Sala de reuniones</option>
-		          <option>Proyector portátil</option>
-	        	</select>
-	        <br/>
-	          <select class="form-control" id="estado" name="estado" onChange="this.style.backgroundColor=this.options[this.selectedIndex].style.backgroundColor">
-		          <option style="background-color: white; color: black;" disabled selected>Estado</option>
-		          <option style="background-color: #A9F5BC; color: black;">Disponible</option>
-		          <option style="background-color: #F78181; color: black;">Reservado</option>
-		          <option style="background-color: #E6E6E6; color: black;">Devuelto</option>
-	          </select>
-	        </br>
-	        <button type="submit" class="btn btn-default" style="margin: auto; width: 58%; background-color:white!important;border: 1px solid #ced4da!important;">Filtrar</button>
-	        <button type="reset" value="reset" class="btn btn-default" style="float:right; margin: auto; width: 40%; background-color:white!important;border: 1px solid #ced4da!important;" onClick="document.getElementById('estado').style.backgroundColor='#fff'">Limpiar</button>
-      	</form>
+      <form>
+        <div class="form-group w3-bar-item">
+          <label for="usr">Name:</label>
+          <input type="text" class="form-control" id="usr">
+          <br/>
+          <select class="form-control" id="selector 1">
+          <option disabled selected>Selecciona un tipo</option>
+          <option>Aula de teoría</option>
+          <option>Aula Informatica</option>
+          <option>Portatil</option>
+          <option>Despacho</option>
+          <option>Carro de portátil</option>
+          <option>Dispositivo Móvil</option>
+          <option>Sala de reuniones</option>
+          <option>Proyector portátil</option>
+        </select>
+        <br/>
+          <select class="form-control" id="selector 2" onChange="this.style.backgroundColor=this.options[this.selectedIndex].style.backgroundColor">
+          <option style="background-color: white; color: black;" disabled selected>Estado</option>
+          <option style="background-color: #A9F5BC; color: black;">Disponible</option>
+          <option style="background-color: #F78181; color: black;">Reservado</option>
+          <option style="background-color: #E6E6E6; color: black;">Devuelto</option>
+          </select>
+        </br>
+        <button type="button" class="btn btn-default" style="margin: auto; width: 58%; background-color:white!important;border: 1px solid #ced4da!important;">Filtrar</button>
+        <button type="reset" value="reset" class="btn btn-default" style="float:right; margin: auto; width: 40%; background-color:white!important;border: 1px solid #ced4da!important;" onClick="document.getElementById('selector 2').style.backgroundColor='#fff'">Limpiar</button>
+      </form>
   </nav>
 
   <!-- Page Content -->
@@ -79,11 +106,14 @@
     <div class="row">
     <?php
       // CONEXION A LA BBDD
-      $conexion = mysqli_connect("localhost","root","","colegiodno");
+      $conexion = mysqli_connect("localhost","root","","proyecto_reservas");
       $acentos=mysqli_query($conexion,"SET NAMES 'utf8'");
 
       $query = "SELECT * FROM tbl_recursos";
-      $lanzarquery = mysqli_query($conexion,$query);      
+      $lanzarquery = mysqli_query($conexion,$query);
+
+
+      
       while($resultado=mysqli_fetch_array($lanzarquery)){
         if($resultado['recurso_estado']=="Disponible"){
           echo "<div class='col-lg-3 col-md-4 col-sm-6 portfolio-item'>";
@@ -94,11 +124,9 @@
                     echo "ID: $resultado[recurso_id] ";
                     echo "<a href='#'>$resultado[recurso_nombre]</a>";
                   echo "</h4>";
-                echo "<h6>($resultado[recurso_tipo])</h6>";
-                echo "<form action='index_reservar.php'>";
-                	echo "<input type='hidden' name='id_recurso' value='$resultado[recurso_id]'>";
-                	echo "<button type='submit' class='btn btn-success boton'>Reservar</button>";
-                echo "</form>";	
+                echo "<h6>($resultado[recurso_tipo])</h6>"; 
+                // echo "<p class='card-text'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur eum quasi sapiente nesciunt?</p>";
+                echo "<button type='button' class='btn btn-success boton'>Reservar</button>";
               echo "</div>";
             echo "</div>";
           echo "</div>";
@@ -125,11 +153,9 @@
                     echo " (";
                     echo date ('G:i:s', strtotime($resultado2['reservarecurso_fechareserva']));
                     echo ") </br>";
-                    echo "<form action='index_devolver.php'>";
-                    	echo "<input type='hidden' name='id_reserva' value='$resultado2[reservarecurso_id]'>";
-                    	echo "<button type='submit' class='btn btn-danger boton'>Devolver</button>";
-                    echo "</form>"; 
                   }
+                // echo "<p class='card-text'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur eum quasi sapiente nesciunt?</p>";
+                echo "<button type='button' class='btn btn-danger boton'>Devolver</button>";
               echo "</div>";
             echo "</div>";
           echo "</div>";
